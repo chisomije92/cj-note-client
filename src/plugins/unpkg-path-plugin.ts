605452d247a451d18e5eed6e48eac9363f42ea00
Chisom <chisomije92@gmail.com>
2022-05-10 12:17:54 +0100

    dynamically fetching modules

--- src/plugins/unpkg-path-plugin.ts
+++ src/plugins/unpkg-path-plugin.ts
@@ -1,3 +1,4 @@
+import axios from "axios";
 import * as esbuild from "esbuild-wasm";
 
 export const unpkgPathPlugin = () => {
@@ -6,7 +7,14 @@ export const unpkgPathPlugin = () => {
     setup(build: esbuild.PluginBuild) {
       build.onResolve({ filter: /.*/ }, async (args: any) => {
         console.log("onResolve", args);
-        return { path: args.path, namespace: "a" };
+        if (args.path === "index.js") {
+          return { path: args.path, namespace: "a" };
+        } else if (args.path === "tiny-test-pkg") {
+          return {
+            path: `https://unpkg.com/${args.path}`,
+            namespace: "a",
+          };
+        }
       });
 
       build.onLoad({ filter: /.*/ }, async (args: any) => {
@@ -16,14 +24,15 @@ export const unpkgPathPlugin = () => {
           return {
             loader: "jsx",
             contents: `
-              import message from './message';
+             const message = require('tiny-test-pkg');
               console.log(message);
             `,
           };
         } else {
+          let { data } = await axios.get(args.path);
           return {
             loader: "jsx",
-            contents: 'export default "hi there!"',
+            contents: data,
           };
         }
       });
