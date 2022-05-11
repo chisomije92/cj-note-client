a62643fb4d1920d06a5bd64015ddd516c4a2b54c
Chisom <chisomije92@gmail.com>
2022-05-11 12:49:28 +0100

    refactor resolve events to include different file paths

--- src/plugins/unpkg-path-plugin.ts
+++ src/plugins/unpkg-path-plugin.ts
@@ -10,20 +10,17 @@ export const unpkgPathPlugin = (input: string) => {
   return {
     name: "unpkg-path-plugin",
     setup(build: esbuild.PluginBuild) {
+      build.onResolve({ filter: /(^index\.js$)/ }, async (args: any) => {
+        return { path: "index.js", namespace: "a" };
+      });
+
+      build.onResolve({ filter: /\.\/+/ }, async (args: any) => {
+        return {
+          path: new URL(args.path, `https://unpkg.com${args.resolveDir}/`).href,
+          namespace: "a",
+        };
+      });
       build.onResolve({ filter: /.*/ }, async (args: any) => {
-        console.log("onResolve", args);
-        if (args.path === "index.js") {
-          return { path: args.path, namespace: "a" };
-        }
-
-        if (args.path.includes("./") || args.path.includes("../")) {
-          return {
-            path: new URL(args.path, `https://unpkg.com/${args.resolveDir}/`)
-              .href,
-            namespace: "a",
-          };
-        }
-
         return {
           path: `https://unpkg.com/${args.path}`,
           namespace: "a",
