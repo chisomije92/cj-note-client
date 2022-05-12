fc97b9fe1b50832848187f6584fe7e522959d6d0
Chisom <chisomije92@gmail.com>
2022-05-12 17:33:02 +0100

    configure prettier to format editor contents

--- src/components/code-editor.tsx
+++ src/components/code-editor.tsx
@@ -1,5 +1,7 @@
-import Editor from "@monaco-editor/react";
+import Editor, { OnChange } from "@monaco-editor/react";
 import React, { useRef } from "react";
+import prettier from "prettier";
+import parser from "prettier/parser-babel";
 
 interface CodeEditorProps {
   defaultValue: string;
@@ -7,37 +9,58 @@ interface CodeEditorProps {
 }
 
 const CodeEditor: React.FC<CodeEditorProps> = ({ defaultValue, passValue }) => {
-  const editorRef = useRef<any>(null);
+  const editorRef = useRef<any>();
 
-  //   function handleEditorDidMount(editor: any, monaco: any) {
-  //     editorRef.current = editor;
-  //     onChange(editor.current.getValue());
-  //   }
-
-  function handleEditorChange(value: any, event: any) {
-    // console.log("here is the current model value:", value);
-    passValue(value);
+  function handleEditorDidMount(editor: any, monaco: any) {
+    editorRef.current = editor;
+    // console.log(editorRef.current);
   }
+
+  const handleEditorChange: OnChange = (value, event) => {
+    // console.log("here is the current model value:", value);
+    if (value) {
+      passValue(value);
+    }
+  };
+
+  const onFormatClick = () => {
+    // console.log(editorRef.current);
+    const unformatted = editorRef.current.getModel().getValue();
+    const formatted = prettier
+      .format(unformatted, {
+        parser: "babel",
+        plugins: [parser],
+        useTabs: false,
+        semi: true,
+        singleQuote: true,
+      })
+      .trim();
+    editorRef.current.setValue(formatted);
+  };
   return (
-    <Editor
-      defaultValue={defaultValue}
-      //   onMount={handleEditorDidMount}
-      onChange={handleEditorChange}
-      height="500px"
-      language="javascript"
-      theme="vs-dark"
-      options={{
-        wordWrap: "on",
-        minimap: {
-          enabled: false,
-        },
-        showUnused: false,
-        lineNumbersMinChars: 3,
-        fontSize: 16,
-        scrollBeyondLastLine: false,
-        automaticLayout: true,
-      }}
-    />
+    <div>
+      <button onClick={onFormatClick}>Format</button>
+      <Editor
+        defaultValue={defaultValue}
+        onMount={handleEditorDidMount}
+        onChange={handleEditorChange}
+        height="500px"
+        language="javascript"
+        theme="vs-dark"
+        options={{
+          wordWrap: "on",
+          minimap: {
+            enabled: false,
+          },
+          showUnused: false,
+          lineNumbersMinChars: 3,
+          fontSize: 16,
+          scrollBeyondLastLine: false,
+          automaticLayout: true,
+          tabSize: 2,
+        }}
+      />
+    </div>
   );
 };
 
