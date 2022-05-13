14110ec5fa971033c774b041c1e313877d9aa0a8
Chisom <chisomije92@gmail.com>
2022-05-13 21:44:16 +0100

    remove useeffect from app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,18 @@
 import "bulmaswatch/superhero/bulmaswatch.min.css";
-import { useEffect, useRef } from "react";
+
 import { startService } from "./bundler";
 import CodeCell from "./components/code-cell";
 
 function App() {
-  const ref = useRef<any>(null);
-
-  useEffect(() => {
-    if (!ref.current) {
-      setTimeout(async () => {
-        await startService();
-        ref.current = true;
-      }, 10);
-    }
-    return;
-  }, []);
+  // useEffect(() => {
+  //   if (!ref.current) {
+  //     setTimeout(async () => {
+  //       await startService().then(() => {});
+  //       ref.current = true;
+  //     }, 0);
+  //   }
+  // }, []);
+  startService();
   return (
     <div className="App">
       <CodeCell />
