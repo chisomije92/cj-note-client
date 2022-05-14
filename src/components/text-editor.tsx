f1995314fc4f3993f6430bb7872443f65253599b
Chisom <chisomije92@gmail.com>
2022-05-14 15:01:03 +0100

    configure value state for mdeditor

--- src/components/text-editor.tsx
+++ src/components/text-editor.tsx
@@ -1,11 +1,38 @@
 import MDEditor from "@uiw/react-md-editor";
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
 
 const TextEditor: React.FC = () => {
+  const ref = useRef<HTMLDivElement | null>(null);
+  const [editing, setEditing] = useState(false);
+  const [value, setValue] = useState("# Header");
+
+  useEffect(() => {
+    const listener = (event: MouseEvent) => {
+      if (
+        ref.current &&
+        event.target &&
+        ref.current.contains(event.target as Node)
+      ) {
+        return;
+      }
+      setEditing(false);
+    };
+    document.addEventListener("click", listener, { capture: true });
+    return () => {
+      document.removeEventListener("click", listener, { capture: true });
+    };
+  }, []);
+
+  if (editing) {
+    return (
+      <div ref={ref}>
+        <MDEditor value={value} onChange={(v) => setValue(v || "")} />
+      </div>
+    );
+  }
   return (
-    <div>
-      {/* <MDEditor /> */}
-      <MDEditor.Markdown source={"# Header"} />
+    <div onClick={() => setEditing(true)}>
+      <MDEditor.Markdown source={value} />
     </div>
   );
 };
