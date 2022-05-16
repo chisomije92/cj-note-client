2c1768336c24ac1949c2ce50a02d43d87c744e9f
Chisom <chisomije92@gmail.com>
2022-05-16 13:43:07 +0100

    add reducers to createslice

--- src/state/reducers/cellReducers.ts
+++ src/state/reducers/cellReducers.ts
@@ -1,5 +1,9 @@
 import { createSlice, PayloadAction } from "@reduxjs/toolkit";
-import { MoveCellActionModel } from "../action-models";
+import {
+  InsertCellBeforeActionModel,
+  MoveCellActionModel,
+  UpdateCellActionModel,
+} from "../action-models";
 import { Cell } from "../cell";
 
 interface CellState {
@@ -23,7 +27,53 @@ const cellsSlice = createSlice({
   initialState,
   reducers: {
     moveCell(state: CellState, action: PayloadAction<MoveCellActionModel>) {
-      const { id, direction } = action.payload;
+      const { direction } = action.payload;
+      const index = state.order.findIndex((id) => id === action.payload.id);
+      const targetIndex = direction === "up" ? index - 1 : index + 1;
+      if (targetIndex < 0 || targetIndex > state.order.length - 1) {
+        return state;
+      }
+      state.order[index] = state.order[targetIndex];
+      state.order[targetIndex] = action.payload.id;
+      return state;
+    },
+    insertCellBefore(
+      state: CellState,
+      action: PayloadAction<InsertCellBeforeActionModel>
+    ) {
+      const cell: Cell = {
+        content: "",
+        type: action.payload.type,
+        id: randomId(),
+      };
+
+      state.data[cell.id] = cell;
+
+      const foundIndex = state.order.findIndex(
+        (id) => id === action.payload.id
+      );
+      if (foundIndex < 0) {
+        state.order.push(cell.id);
+      } else {
+        state.order.splice(foundIndex, 0, cell.id);
+      }
+      return state;
+    },
+    updateCell(state: CellState, action: PayloadAction<UpdateCellActionModel>) {
+      const { id, content } = action.payload;
+      state.data[id].content = content;
+      return state;
+    },
+    deleteCell(state: CellState, action: PayloadAction<string>) {
+      delete state.data[action.payload];
+      state.order = state.order.filter((id) => id !== action.payload);
+      return state;
     },
   },
 });
+
+export const cellsSliceActions = cellsSlice.actions;
+
+const randomId = () => {
+  return Math.random().toString(36).substr(2, 5);
+};
