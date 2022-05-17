0d7eaf7063e09551fda1f77ff3557de0272e301c
Chisom <chisomije92@gmail.com>
2022-05-17 23:04:22 +0100

    implement createBundle thunk for handling async bundling

--- src/state/reducers/index.ts
+++ src/state/reducers/index.ts
@@ -1,17 +1,34 @@
-import { configureStore } from "@reduxjs/toolkit";
-import bundleSlice from "./bundleReducers";
+import { configureStore, MiddlewareArray } from "@reduxjs/toolkit";
+import bundleSlice, { bundleSliceActions } from "./bundleReducers";
+import { AnyAction, Dispatch } from "redux";
 import cellsSlice from "./cellReducers";
+import thunk from "redux-thunk";
+import { ThunkAction } from "@reduxjs/toolkit";
+import { bundler } from "../../bundler";
 
 const store = configureStore({
   reducer: {
     cells: cellsSlice.reducer,
     bundle: bundleSlice.reducer,
   },
+  middleware: new MiddlewareArray().concat(thunk),
 });
+
 export default store;
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
+
+export const createBundle = (
+  cellId: string,
+  input: string
+): ThunkAction<void, RootState, unknown, AnyAction> => {
+  return async (dispatch) => {
+    dispatch(bundleSliceActions.bundleStart({ cellId }));
+    const result = await bundler(input);
+    dispatch(bundleSliceActions.bundleComplete({ cellId, bundle: result }));
+  };
+};
 
 // store.dispatch({
 //   type: "cells/insertCellBefore",
