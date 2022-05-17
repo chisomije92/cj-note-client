7d99f6d86a0f4af139a0e4103ea7f02e88d921e2
Chisom <chisomije92@gmail.com>
2022-05-17 11:21:06 +0100

    implement a typed selector

--- src/components/cell-list.tsx
+++ src/components/cell-list.tsx
@@ -1,6 +1,8 @@
 import React from "react";
+import { useTypedSelector } from "../state/reducers/use-typed-selector";
 
 const CellList = () => {
+  const cells = useTypedSelector((state) => state);
   return <div>CellList</div>;
 };
 
